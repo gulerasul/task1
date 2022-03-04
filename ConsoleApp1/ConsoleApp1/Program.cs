798dc528b931768590f0ea6f3f86e4be0965dc82
Guler Resulova <tu699evkp@aztu.local>
2022-03-04 15:39:08 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string months = Console.ReadLine();
+
+            switch (months)
+            {
+                case "yanvar":
+                case "mart":
+                case "may":
+                case "iyul":
+                case "avqust":
+                case "oktyabr":
+                case "dekabr":
+                    Console.WriteLine(31);
+                    break;
+                case "aprel":
+                case "iyun":
+                case "sentyabr":
+                case "noyabr":
+                    Console.WriteLine(30);
+                    break;
+                case "fevral":
+                    Console.WriteLine(28);
+                    break;
+               
+                default:
+                    Console.WriteLine( "bele gun yoxdur ");
+                    break;
+            }
+
+
+        }
+    }
+}
